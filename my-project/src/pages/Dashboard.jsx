9f0316c506a8bfb8191fefe3fcf97c395f4a38dd
Dhanushapathum <dhanushapathum3@gmail.com>
2026-06-25 12:45:09 +0530

    change sidebar

--- my-project/src/pages/Dashboard.jsx
+++ my-project/src/pages/Dashboard.jsx
@@ -1,8 +1,32 @@
-import { FiBell } from "react-icons/fi";
+import {
+  FiBell,
+  FiHome,
+  FiUsers,
+  FiBookOpen,
+  FiBriefcase,
+  FiUser,
+  FiCalendar,
+  FiClipboard,
+  FiBarChart2,
+  FiDollarSign,
+  FiFileText,
+  FiSettings,
+  FiLogOut,
+  FiGrid,
+} from "react-icons/fi";
+import {useNavigate} from "react-router-dom";
+
+
 import AdminImage from '../assets/admin.png';
 
 
 function Dashboard() {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    // Logout logic
+    navigate("/");
+  }
+    
   return (
     <div className="dashboard">
             
@@ -10,18 +34,23 @@ function Dashboard() {
                 <div className="sidebar">
                     <h1>STUDENT</h1>
                     <h2>MANAGEMENT SYSTEM</h2>
-                    <ul> 
-                        <li> Dashboard</li>
-                        <li> Students</li>
-                        <li> Courses</li>
-                        <li> Departments</li>
-                        <li> Teachers</li>
-                        <li> Attendance</li>
-                        <li> Exams</li>
-                        <li> Results</li>
-                        <li> Fees</li>
-                        <li> Reports</li>
-                        <li> Settings</li>
+                    <ul>
+                        <li><FiHome /> <span>Dashboard</span></li>
+                    
+                        <li><FiUsers /> <span>Students</span></li>
+                        <li><FiBookOpen /> <span>Courses</span></li>
+                        <li><FiBriefcase /> <span>Departments</span></li>
+                        <li><FiUser /> <span>Teachers</span></li>
+                        <li><FiCalendar /> <span>Attendance</span></li>
+                        <li><FiClipboard /> <span>Exams</span></li>
+                        <li><FiBarChart2 /> <span>Results</span></li>
+                        <li><FiDollarSign /> <span>Fees</span></li>
+                        <li><FiFileText /> <span>Reports</span></li>
+                        <li><FiSettings /> <span>Settings</span></li>
+                        <div className="logout">
+                             <li onClick={handleLogout}><FiLogOut /> <span>Logout</span></li>
+                        </div>
+                       
                     </ul>
                 </div>
 
